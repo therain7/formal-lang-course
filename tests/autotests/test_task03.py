f593ef15a6ff6137f8bcea969a74d4d676a65a9d
Andrei <therain.i@yahoo.com>
2024-09-18 03:09:54 +0300

    test: enable tests for task3

--- tests/autotests/test_task03.py
+++ tests/autotests/test_task03.py
@@ -12,8 +12,9 @@ from rpq_concrete_cases import CASES_RPQ, CaseRPQ
 
 # Fix import statements in try block to run tests
 try:
-    from project.task2 import regex_to_dfa
-    from project.task3 import intersect_automata, AdjacencyMatrixFA, tensor_based_rpq
+    from project.regex_tools import regex_to_dfa
+    from project.fa import intersect_automata, AdjacencyMatrixFA
+    from project.rpq import tensor_based_rpq
 except ImportError:
     pytestmark = pytest.mark.skip("Task 3 is not ready to test!")
 
