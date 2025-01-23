b0cf0503f8bd1303c22e68df085c3d0c8443a1b2
Andrei <therain.i@yahoo.com>
2025-01-23 12:57:25 +0300

    feat(cfpq): impl `matrix_based_cfpq`

--- project/cfpq.py
+++ project/cfpq.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
 from dataclasses import dataclass
+from itertools import product
 from typing import Iterable, Optional, cast
 
 import networkx as nx
 from pyformlang.cfg import CFG, Production, Terminal, Variable
+from scipy.sparse import csr_array
 
 from project.grammar_tools import cfg_to_weak_normal_form
 
@@ -84,4 +86,54 @@ def hellings_based_cfpq(
         (e.start, e.end)
         for e in edges
         if e.var == cfg.start_symbol and e.start in start_nodes and e.end in final_nodes
+    }
+
+
+def matrix_based_cfpq(
+    cfg: CFG,
+    graph: nx.DiGraph,
+    start_nodes: Optional[set[int]] = None,
+    final_nodes: Optional[set[int]] = None,
+) -> set[tuple[int, int]]:
+    nodes = {int(n): idx for (idx, n) in enumerate(graph.nodes)}
+    start_nodes = start_nodes if start_nodes else set(nodes.keys())
+    final_nodes = final_nodes if final_nodes else set(nodes.keys())
+
+    cfg = cfg_to_weak_normal_form(cfg)
+    prods = ReversedProds(cfg.productions)
+
+    matrices: dict[Variable, csr_array] = defaultdict(
+        lambda: csr_array((n := len(nodes), n), dtype=bool)
+    )
+
+    for v1, v2, lbl in graph.edges(data="label"):
+        for hd in prods.terminals.get(Terminal(lbl), set()):
+            matrices[hd][nodes[v1], nodes[v2]] = True
+
+    for var in cfg.get_nullable_symbols():
+        matrices[cast(Variable, var)].setdiag(True)
+
+    updated_vars = list(cfg.variables)
+    while updated_vars:
+        updated = updated_vars.pop(0)
+
+        for body, heads in prods.bodies.items():
+            if updated not in body:
+                continue
+
+            delta: csr_array = matrices[body[0]] @ matrices[body[1]]
+            for hd in heads:
+                old = matrices[hd]
+                matrices[hd] += delta
+
+                if old.nnz < matrices[hd].nnz:
+                    updated_vars.append(hd)
+
+    if cfg.start_symbol not in matrices:
+        return set()
+
+    return {
+        (start, final)
+        for (start, final) in product(start_nodes, final_nodes)
+        if matrices[cfg.start_symbol][nodes[start], nodes[final]]
     }
